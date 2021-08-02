6734e63071d23022da9944f425a6c4ed100e289d
GrandmasterSivaram <nsivaramdav@gmail.com>
2021-08-02 11:41:48 +0200

    Finished with filescreen UI

--- lib/screens/files_screen.dart
+++ lib/screens/files_screen.dart
@@ -9,108 +9,121 @@ class FilesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       child: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
+        child: Stack(
           children: [
-            SizedBox(
-              height: 30,
+            SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Recent Files",
+                      style: textStyle(20, textColor, FontWeight.bold),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Container(
+                    height: 100,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 5,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(right: 15.0),
+                          child: Column(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(18),
+                                child: Image(
+                                  width: 70,
+                                  height: 70,
+                                  image: NetworkImage(
+                                      'https://www.nyhabitat.com/de/blog/wp-content/uploads/2013/02/Times-Square-Manhattan-New-York-NYC-Kreuzung-Welt.jpg'),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "Home.jpg",
+                                style:
+                                    textStyle(13, textColor, FontWeight.w500),
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  GridView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: 8,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 20,
+                        mainAxisSpacing: 20),
+                    itemBuilder: (context, index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.grey.shade100,
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey.withOpacity(0.00001),
+                                offset: Offset(10, 10),
+                                blurRadius: 5),
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'images/folder.png',
+                              width: 100,
+                              height: 100,
+                              fit: BoxFit.cover,
+                            ),
+                            Text(
+                              "4k movies",
+                              style: textStyle(18, textColor, FontWeight.bold),
+                            ),
+                            Text(
+                              "911 Items",
+                              style: textStyle(
+                                  14, Colors.grey[400], FontWeight.bold),
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
             Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Recent Files",
-                style: textStyle(20, textColor, FontWeight.bold),
-              ),
-            ),
-            SizedBox(height: 15),
-            Container(
-              height: 100,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 5,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(right: 15.0),
-                    child: Column(
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(18),
-                          child: Image(
-                            width: 70,
-                            height: 70,
-                            image: NetworkImage(
-                                'https://www.nyhabitat.com/de/blog/wp-content/uploads/2013/02/Times-Square-Manhattan-New-York-NYC-Kreuzung-Welt.jpg'),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          "Home.jpg",
-                          style: textStyle(13, textColor, FontWeight.w500),
-                        )
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            GridView.builder(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: 4,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, crossAxisSpacing: 20, mainAxisSpacing: 20),
-              itemBuilder: (context, index) {
-                return Container(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 10.0),
+                child: Container(
+                  width: 50,
+                  height: 50,
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.grey.shade100,
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey.withOpacity(0.00001),
-                          offset: Offset(10, 10),
-                          blurRadius: 5),
-                    ],
+                      color: Colors.redAccent[200],
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Center(
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 32,
+                    ),
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'images/folder.png',
-                        width: 100,
-                        height: 100,
-                        fit: BoxFit.cover,
-                      ),
-                      Text(
-                        "4k movies",
-                        style: textStyle(18, textColor, FontWeight.bold),
-                      ),
-                      Text(
-                        "911 Items",
-                        style: textStyle(14, Colors.grey[400], FontWeight.bold),
-                      )
-                    ],
-                  ),
-                );
-              },
-            ),
-            Container(
-              width: 50,
-              height: 50,
-              decoration: BoxDecoration(
-                  color: Colors.redAccent[200],
-                  borderRadius: BorderRadius.circular(10)),
-              child: Center(
-                child: Icon(
-                  Icons.add,
-                  color: Colors.white,
-                  size: 32,
                 ),
               ),
             )
