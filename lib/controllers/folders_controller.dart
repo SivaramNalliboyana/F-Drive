f64bfd9902148bca18cebe23398738e041f84e5c
GrandmasterSivaram <nsivaramdav@gmail.com>
2021-08-05 12:39:59 +0200

    Added usercollection and ability to create folders

---
+++ lib/controllers/folders_controller.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fdrive/models/folder_model.dart';
+import 'package:fdrive/utils/utils.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+class FoldersController extends GetxController {
+  String uid = FirebaseAuth.instance.currentUser.uid;
+  RxList<FolderModel> foldersList = <FolderModel>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    foldersList.bindStream(
+      userCollection.doc(uid).collection('folders').snapshots().map(
+        (QuerySnapshot query) {
+          List<FolderModel> folders = [];
+          query.docs.forEach((element) {
+            folders.add(FolderModel.fromDocumentSnapshot(element));
+          });
+          return folders;
+        },
+      ),
+    );
+  }
+}
