3558d9e353ca3bef1867010f794fcd399bda61e6
GrandmasterSivaram <nsivaramdav@gmail.com>
2021-08-11 23:49:56 +0200

    Added navigation to options screen

--- lib/widgets.dart/upload_options.dart
+++ lib/widgets.dart/upload_options.dart
@@ -1,14 +1,19 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:fdrive/screens/options_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class UploadOptions extends StatelessWidget {
-  Widget colouredContainer(Color bgcolor, Icon icon) {
-    return Container(
-      width: 50,
-      height: 50,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15), color: bgcolor),
-      child: Center(child: icon),
+  Widget colouredContainer(Color bgcolor, Icon icon, String option) {
+    return InkWell(
+      onTap: () => Get.to(() => OptionsScreen(option)),
+      child: Container(
+        width: 50,
+        height: 50,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15), color: bgcolor),
+        child: Center(child: icon),
+      ),
     );
   }
 
@@ -18,27 +23,27 @@ class UploadOptions extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
         colouredContainer(
-          Colors.lightBlue.withOpacity(0.2),
-          Icon(
-            Icons.image,
-            color: Colors.cyan,
-            size: 30,
-          ),
-        ),
+            Colors.lightBlue.withOpacity(0.2),
+            Icon(
+              Icons.image,
+              color: Colors.cyan,
+              size: 30,
+            ),
+            "Images"),
         colouredContainer(
-          Colors.pink.withOpacity(0.3),
-          Icon(Icons.play_arrow_rounded,
-              color: Colors.pink.withOpacity(0.5), size: 42),
-        ),
+            Colors.pink.withOpacity(0.3),
+            Icon(Icons.play_arrow_rounded,
+                color: Colors.pink.withOpacity(0.5), size: 42),
+            "Videos"),
         colouredContainer(
-          Colors.blue.withOpacity(0.4),
-          Icon(EvaIcons.fileText,
-              color: Colors.indigoAccent.withOpacity(0.5), size: 30),
-        ),
+            Colors.blue.withOpacity(0.4),
+            Icon(EvaIcons.fileText,
+                color: Colors.indigoAccent.withOpacity(0.5), size: 30),
+            "Documents"),
         colouredContainer(
-          Colors.purple.withOpacity(0.2),
-          Icon(EvaIcons.music, color: Colors.pink.withOpacity(0.3), size: 30),
-        ),
+            Colors.purple.withOpacity(0.2),
+            Icon(EvaIcons.music, color: Colors.pink.withOpacity(0.3), size: 30),
+            "Audios"),
       ],
     );
   }
